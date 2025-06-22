b9c89aeb34f62d76b3232b82a22dfc072b7ecbd4
Sagar Addagarla <sagar_csd@srkrec.edu.in>
2025-06-22 16:00:44 +0530

    added the react files

---
+++ Course/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+  );
+}
+
+
+export default App
