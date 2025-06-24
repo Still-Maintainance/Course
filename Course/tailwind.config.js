665285861e03f40f01790243436457faad45abd4
Sagar Addagarla <sagar_csd@srkrec.edu.in>
2025-06-24 12:41:14 +0530

    corrected the tailwind config file

--- Course/tailwind.config.js
+++ Course/tailwind.config.js
@@ -1,6 +1,7 @@
+// tailwind.config.js
 export const content = [
-  "./index.html",
-  "./src//*.{js,jsx,ts,tsx}"
+  "./src/**/*.{js,ts,jsx,tsx}", // for React/Vite
+  "./public/index.html" // if needed
 ];
 export const theme = {
   extend: {},
